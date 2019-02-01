c8adad5714854f08dba00ca42418a42ac77ffd4e
Sampath <sampath96v@gmail.com>
2019-01-31 21:53:21 -0600

    Commited by Sampath for 6,7 and 8 questions.

--- SortComparison/frmMain.cs
+++ SortComparison/frmMain.cs
@@ -29,7 +29,16 @@ namespace SortComparison
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.tbSamples.Maximum = 800;
             this.tbSamples.Value = 200;
+            this.cboAlg1.SelectedIndex = 3;
+            this.cboAlg2.SelectedIndex = 12;
+
+            
+            
+
+
+          
         }
 
         public void Randomize(IList list)
